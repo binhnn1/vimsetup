fb27a726726d1141c2c330d4cf3f03e4140cbb64
Edward Nguyen <bnguyen@abaltatech.com>
2019-12-12 11:08:47 -0800

    - Fixed some config. - Added git_commit_msg

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@ echo "Installing vim setup"
 cd ~
 git clone https://github.com/binhnn1/vimsetup.git ~/.vim
 
-ln -sf ~/.vim/vimrc ~/.vimrc
-ln -sf ~/.vim/zsh/zshrc ~/.zshrc
-ln -sf ~/.vim/gitconf/gitconfig  ~/.gitconfig
-
 cd ~/.vim
 git submodule update --init --recursive
 
@@ -17,3 +13,9 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 
 echo "Installing powerlevel9k theme for ohmyzsh"
 ln -sf ~/.vim/zsh/powerlevel9k/ ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+ln -sf ~/.vim/vimrc ~/.vimrc
+ln -sf ~/.vim/gitconf/gitconfig  ~/.gitconfig
+
+#Run this after restart shell
+#ln -sf ~/.vim/zsh/zshrc ~/.zshrc
