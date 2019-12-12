88e3de9d6e93d4c4352d0f4ad53cf3643b6d717e
Binh Nguyen <binhnn1@uci.edu>
2019-12-12 00:57:47 -0800

    - Added install zshell - Added install oh my zsh - Added install powerlevel9k for zshell - Added gitconfig

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
+echo "Installing vim setup"
 cd ~
 git clone https://github.com/binhnn1/vimsetup.git ~/.vim
-ln -s ~/.vim/vimrc ~/.vimrc
+
+ln -sf ~/.vim/vimrc ~/.vimrc
+ln -sf ~/.vim/zsh/zshrc ~/.zshrc
+ln -sf ~/.vim/gitconf/gitconfig  ~/.gitconfig
+
 cd ~/.vim
 git submodule update --init --recursive
+
+echo "Installing zsh"
+sudo apt install zsh
+
+echo "Installing ohmyzsh"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing powerlevel9k theme for ohmyzsh"
+ln -sf ~/.vim/zsh/powerlevel9k/ ~/.oh-my-zsh/custom/themes/powerlevel9k
