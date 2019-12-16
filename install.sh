92e07a01a3368ec528564a919d84e8949a62b648
Binh Nguyen <binhnn1@uci.edu>
2019-12-15 23:05:44 -0800

    - Installed powerlevel10k - Removed powerlevel9k

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ sudo apt install zsh
 echo "Installing ohmyzsh"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Installing powerlevel9k theme for ohmyzsh"
-ln -sf ~/.vim/zsh/powerlevel9k/ ~/.oh-my-zsh/custom/themes/powerlevel9k
+echo "Installing powerlevel10k theme for ohmyzsh"
+ln -sf ~/.vim/zsh/powerlevel10k/ ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/gitconf/gitconfig  ~/.gitconfig
@@ -20,15 +20,12 @@ ln -sf ~/.vim/gitconf/gitconfig  ~/.gitconfig
 #Run this after restart shell
 #ln -sf ~/.vim/zsh/zshrc ~/.zshrc
 
-echo "Installing powerline font for powerlevel9k"
+echo "Installing powerline font for powerlevel10k"
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 
-FONT_PATH=/usr/share/fonts/X11/misc
-FONT_CONFIG=~/.config/fontconfig/conf.d
+sudo mv PowerlineSymbols.otf /usr/share/fonts/X11/misc/
+fc-cache -vf /usr/share/fonts/X11/misc
 
-sudo mv PowerlineSymbols.otf FONT_PATH
-fc-cache -vf FONT_PATH
-
-mkdir -p FONT_CONFIG
-mv 10-powerline-symbols.conf FONT_CONFIG
+mkdir -p ~/.config/fontconfig/conf.d
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d
